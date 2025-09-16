2c3b8fb6da77defc2fabb94ad21526bd9d5215f7
Md Siddique Ahmad <md-ahmad@newgensoft.com>
2025-09-17 01:10:58 +0530

    updated

--- app/(journey)/creditCard/submitApplication/counterOffer.tsx
+++ app/(journey)/creditCard/submitApplication/counterOffer.tsx
@@ -46,8 +46,8 @@ const CounterOffer = () => {
       onClose={() => router.back()}
       doubleButtonTitle1={"Need higher amount"}
       doubleButtonTitle2={"Accept Offer"}
-      onPressDoubleButton1={() => {}}
-      onPressDoubleButton2={() => {}}
+      onPressDoubleButton1={() => router.push("/(journey)/creditCard/submitApplication/applicationApproved")}
+      onPressDoubleButton2={() => router.push("/(journey)/creditCard/submitApplication/congratulations")}
     >
       <View style={styles.image}>
         <Image
