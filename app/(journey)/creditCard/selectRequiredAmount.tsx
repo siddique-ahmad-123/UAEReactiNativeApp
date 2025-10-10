7b31b0f312cf31b25f09ae2e3cb07eabf7a7a7af
Md Siddique Ahmad <md-ahmad@newgensoft.com>
2025-10-10 19:18:52 +0530

    updated

--- app/(journey)/creditCard/selectRequiredAmount.tsx
+++ app/(journey)/creditCard/selectRequiredAmount.tsx
@@ -1,3 +1,4 @@
+import CustomCheckbox from "@/components/CustomCheckbox/customCheckbox";
 import CustomMainChild from "@/components/CustomMainChild/CustomMainChild";
 import DynamicSliderCard from "@/components/CustomSliderCard/DynamicSliderCard";
 import DocumentDownload from "@/components/DocumentDownload";
@@ -5,6 +6,7 @@ import { fontSize, spacingVertical } from "@/constants/Metrics";
 import { fieldNames } from "@/schemas/creditCard/allFieldNames";
 import { useApplicationStore } from "@/store/applicationStore";
 import Checkbox from "expo-checkbox";
+import { t } from "i18next";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { StyleSheet, Text, View } from "react-native";
@@ -41,7 +43,7 @@ const SelectRequiredAmount = () => {
       flexDirection: "row",
       alignItems: "center",
       marginTop: 20,
-       marginVertical: 10,
+      marginVertical: 10,
       gap: 8,
     },
     checkboxLabel: {
@@ -92,7 +94,7 @@ const SelectRequiredAmount = () => {
           documentName="Download Key Fact Statement"
         />
 
-        <View style={styles.checkboxContainer}>
+        {/* <View style={styles.checkboxContainer}>
           <Checkbox
             style={styles.checkbox}
             value={isChecked}
@@ -103,8 +105,16 @@ const SelectRequiredAmount = () => {
             I agree with below provided Terms and Conditions, Fees and Charges
             Sheet and Key Fact Statement.
           </Text>
-        </View>
-        <View style={styles.checkboxContainer}>
+        </View> */}
+
+        <CustomCheckbox
+          label="I agree with below provided Terms and Conditions, Fees and Charges
+            Sheet and Key Fact Statement."
+          value={isChecked}
+          onChange={setChecked}
+          checkboxPosition="left"
+        />
+        {/* <View style={styles.checkboxContainer}>
           <Checkbox
             style={styles.checkbox}
             value={isChecked2}
@@ -116,7 +126,13 @@ const SelectRequiredAmount = () => {
               I authorize Newgen Bank to access my Bureau and FTS records
             </Text>
           </View>
-        </View>
+        </View> */}
+        <CustomCheckbox
+          label="I authorize Newgen Bank to access my Bureau and FTS records"
+          value={isChecked2}
+          onChange={setChecked2}
+          checkboxPosition="left"
+        />
       </View>
     </CustomMainChild>
   );
